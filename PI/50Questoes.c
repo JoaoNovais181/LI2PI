042afaf8429fda952cd69e1e9a218d82a00b9e9e
JoaoNovais181 <a96626@alunos.uminho.pt>
2021-03-08 00:46:53 +0000

    50 perguntas

--- PI/50Questoes.c
+++ PI/50Questoes.c
@@ -1,104 +1,107 @@
 #include <stdio.h>
-#define INT_MIN -2147483647
-
-int verifica (int num, int num2){
-    if (num > num2) return num;
-    else return num2;
-}
 
 void um(){
-    int num, maior;
-    maior = 0;
-
-    while (num != 0) {
-        printf("Introduza um número: ");
-        scanf("%d", &num);
-        maior = verifica(num, maior);
+    int max,a;
+    max=scanf("%d",&a);
+    while (a!=0){
+        scanf("%d",&a);
+        if (a>max) max=a;
     }
-
-    printf("O maior número é: %d\n", maior);
+    printf("%d\n",max);
 }
-
 void dois(){
-    int num, soma = 0, total = 0;
-
-    while (num!=0) {
-        printf("Introduza um número: ");
-        scanf("%d", &num);
-        total ++;
-        soma += num;
+    int somatorio,n,a=1;
+    while (a!=0){
+        scanf("%d",&a);
+        somatorio+=a;
+        n++;
     }
-    total--;
-
-    printf("A média é: %.2f\n", (float)soma/total);
+    n--;
+    printf("%d\n",somatorio/n);
 }
-
 void tres(){
-    int num, maior = INT_MIN, maior2 = INT_MIN;
-
-    while (num != 0) {
-        printf("Introduza um número: ");
-        scanf("%d", &num);
-        if ((num >= maior2) && (num< maior)) (maior2 = num);
-        else if (num > maior) {
-            (maior2 = maior);
-            (maior = num);
+    int max,a,max2;
+    max=scanf("%d",&a);
+    while (a!=0){
+        scanf("%d",&a);
+        if (a>max){
+            max2=max;
+            max=a;
         }
     }
-
-    printf("O segundo maior número é: %d\n", maior2);
-
+    printf("%d\n",max2);
 }
 
+
 int bitsUm (unsigned int n){
-    int contador=0;
-    while (n>=1){
-        if (n%2 != 0) ++contador;
-        n = n/2;
+    int c=0;
+    while (n!=0){
+        if (n%2==1) c++;
+        n/=2;
     }
-    printf("%d\n", contador);
-    return contador;
+    return c;
 }
 
 int trailingZ (unsigned int n){
-    if (n == 0) return 32;
-    else { 
-        int contador=0;
-        while (n>0){
-            if (n/2 == 0) n = n/2;
-            else {
-                ++contador;
-                n = n/2;
-            }
-        }
-        return contador;
+    if (n==0) return 32;
+    else{
+    int c=0;
+    while (n%2!=1 && n!=0){
+        c++;
+        n/=2;
     }
+    return c;
+}}
+
+int qDig (unsigned int n){
+    int c=0;
+    while (n>9){
+        n=n/10;
+        c++;
+    }
+    c++;
+    return c; 
 }
 
-int qDig (unsigned int n) {
-    int contador = 0;
-    while (n>0) {
-        ++contador;
-        n = n/10;
+char *mystrcat (char s1[], char s2[]){
+    int i,j;
+    for (i = 0;s1[i]!='\0';i++);
+    for (j = 0;s2[j]!='\0';j++){
+        s1[i+j] = s2[j];
     }
-    return contador;   
-}
-
-char *strcat (char s1[], char s2[]){
-    int i1 , i2;
-
-    for (i1=0 ; s1[i1]!=0 ; i1++);
-    for (i2=0 ; s2[i2]!=0 ; i2++){
-        s1[i1] = s2[i2];
-        i1++;
-    }
-    s1[i1] = 0;
+    s1[i+j]='\0';
     return s1;
 }
 
+char *strcpy (char *dest, char source[]){
+    int i;
 
-int main (){
-    char a[100] = "papapapa", b[] = "pepepe";
-    strcat(a,b);
-    puts(a);
+    for(i=0 ; source[i] != '\0' ; i++) dest[i]=source[i];
+
+    dest[i] = '\0';
+
+    return dest;
+}
+
+int strcmp (char s1[], char s2[]) {
+    int i;
+    for (i = 0; s1[i] != '\0' ; i++) {
+        if (s1[i] > s2[i]) {
+            return 1;
+        }
+        else if (s1[i] < s2[i]) {
+            return (-1);
+        }
+    }
+
+    if (s2[i++] != '\0') {
+        return -1;
+    }
+    return 0;    
+}
+
+int main(){
+    char a[] = "aaaa"; char b[] = "aaaaa";
+    printf("%d\n", strcmp(a,b));
+    return 0;
 }
